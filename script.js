9f298623767b068d346ff29816fe0ce979551b7d
Jaspreet Singh <jaspreet104@yahoo.com>
2021-10-02 19:26:08 +0530

    hi

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 let primeNumbersArray = [];
 let numbers = [];
+let myBool = true; //if you click enter twice it will repeat the already shown prime numbers
 
 function primeNumbers(a) {
-    let beforeDate = Date.now();
+    let beforeDate = Date.now(); //checking time before the caluculation
     console.log(beforeDate);
 
-    function generatingNumbers() {
+    function generatingNumbers() { // generating all numbers below the given input
         let b = a - 1;
         for (i = b; i > 1; i--) {
-            numbers.push(i);
+            numbers.push(i); //pushing numbers below the input in the numbers array
         }
     }
     generatingNumbers();
 
-    function isItPrime(parameter) {
+    function isItPrime(parameter) { // a funtion to check if the parameter is prime or not
         let anyFactor = false
         let checker = Math.floor(Math.sqrt(parameter))
         let counter = 0;
         while (checker - counter > 1) {
             if (parameter % (checker - counter) === 0) {
                 anyFactor = true
-                break;
+                    //break;
             }
             counter++;
         }
@@ -29,7 +30,7 @@ function primeNumbers(a) {
         }
     }
 
-    function calculatingPrime() {
+    function calculatingPrime() { // checking if any number in the array numbers is prime
         for (let i = 0; i < numbers.length; i++) {
             const checkingNumber = numbers[i];
             isItPrime(checkingNumber);
@@ -37,15 +38,21 @@ function primeNumbers(a) {
     }
     calculatingPrime();
 
-    let afterDate = Date.now();
+    let afterDate = Date.now(); //to see the time after the calculation is done
     console.log(afterDate);
 
-    console.log(afterDate - beforeDate);
+    console.log(afterDate - beforeDate); //finding the difference to see the time taken for calculation
 
     document.getElementById('label').innerText = primeNumbersArray;
 }
 
 function clickSubmit() {
-    let input = document.getElementById('number').value;
-    primeNumbers(input);
+    if (myBool === true) {
+        let input = document.getElementById('number').value;
+        primeNumbers(input);
+        myBool = false;
+    } else if (myBool === false) {
+        alert('Do not click submit twice...Re-enter values');
+        window.location.reload();
+    }
 }
